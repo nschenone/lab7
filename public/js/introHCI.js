5ef13805380fe21f84e1de4b7515a9379e3d7195
Nicholas Schenone <nschenone16@gmail.com>
2020-02-20 11:08:41 -0800

    Lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function () {
 	initializePage();
 });
 
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLike);
+}
+
+function clickLike(e) {
+	e.preventDefault();
+	ga('create', 'UA-158830494-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
